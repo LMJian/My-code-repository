59af6f17560d4d65b7f99e749d4800c0ce097693
LMJian <1150696681@qq.com>
2019-11-16 13:17:11 +0800

    2019-11-15-网络编程

--- Linux/11-15-网络编程/client.cc
+++ Linux/11-15-网络编程/client.cc
@@ -7,18 +7,26 @@
 
 int main(int argc,char* argv[])
 {
-  int sock=socket(AF_INET,SOCK_DGRAM,0);
+  //1.创建sock
+  int sock=socket(AF_INET,SOCK_DGRAM,0);//AF_INET表示ipv4地址类型
+                                        //SOCK_DGRAM表示使用udp协议
   if(sock<0){
     perror("socket");
     return 1;
   }
-
+  //客户端一般不需要bind
+  //bind意味着和某个具体的端口号关联在一起
+  //没有bind,操作系统随机分配端口号
+  //服务器必须bind，因为要固定端口号，否则客户端无法链接
+  //客户端如果自己bind,可能你绑定了一个重复的端口号会出现问题
+  
+  //2.准备服务器的信息
   sockaddr_in server_addr;
   server_addr.sin_family=AF_INET;
   server_addr.sin_addr.s_addr=inet_addr(argv[1]);
   server_addr.sin_port=htons(9090);
 
-
+  //3.客户端发送数据
   while(1)
   {
     char buf[1024]={0};
@@ -26,11 +34,12 @@ int main(int argc,char* argv[])
     fflush(stdout);
     scanf("%s",buf);
     sendto(sock,buf,strlen(buf),0,(sockaddr*)&server_addr,sizeof(server_addr));
+    //接收返回结果
     char buf_output[1024]={0};
+    //后面两个参数NULL,表示不关注对端地址
     recvfrom(sock,buf_output,sizeof(buf_output)-1,0,NULL,NULL);
     printf("server resp:%s\n",buf_output);
   }
-
 
   return 0;
 }
--- Linux/11-15-网络编程/server.cc
+++ Linux/11-15-网络编程/server.cc
@@ -8,15 +8,19 @@
 
 int main()
 {
+  //1.
   int sock=socket(AF_INET,SOCK_DGRAM,0);
+  //AF_INET表示IPv4地址类型
+  //SOCK_DGRAM表示使用UDP协议
   if(sock<0){
     perror("socket");
     return 1;
   }
-  sockaddr_in addr;
-  addr.sin_family=AF_INET;
-  addr.sin_addr.s_addr=inet_addr("0.0.0.0");
-  addr.sin_port=htons(9090);
+  //2.绑定IP+端口号
+  sockaddr_in addr;//IPv4编程使用该结构
+  addr.sin_family=AF_INET;//表示IPv4
+  addr.sin_addr.s_addr=inet_addr("0.0.0.0");//绑定端口号,该函数自动转为网络字节序
+  addr.sin_port=htons(9090);//将端口号转为网络字节序
   int ret=bind(sock,(sockaddr*)&addr,sizeof(addr));
   if(ret<0)
   {
@@ -24,18 +28,22 @@ int main()
     return 1;
   }
   printf("server start ok!\n");
+  
+  //3.处理请求（读取，响应，写回）
   while(true){
-    char buf[1024]={0};
-    sockaddr_in peer;
-    socklen_t len=sizeof(peer);
+    //a.读取
+    char buf[1024]={0};//保存读取的数据
+    sockaddr_in peer;//客户端ip+端口
+    socklen_t len=sizeof(peer);//输入输出型参数
+    //面向数据报的函数接口
     ssize_t n=recvfrom(sock,buf,sizeof(buf)-1,0,(sockaddr*)&peer,&len);
     if(n<0){
       perror("recvfrom");
       continue;
     }
     buf[n]='\0';
-
-
+    //b.响应
+    //c.写回
     n=sendto(sock,buf,strlen(buf),0,(sockaddr*)&peer,len);
     if(n<0)
     {
