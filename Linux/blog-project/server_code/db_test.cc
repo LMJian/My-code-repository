c5a179a21f032d5ba1c3ace5bdcb8810c0bcdee5
LMJian <1150696681@qq.com>
2020-01-03 21:04:10 +0800

    2020-1-3-更新了前端的代码

--- Linux/blog-project/server_code/db_test.cc
+++ Linux/blog-project/server_code/db_test.cc
@@ -1,25 +1,23 @@
 #include"db.hpp"
 
-
-void TestBlogTable(){
+void TestBlogTable(){  
   Json::StyledWriter writer;
   MYSQL* mysql=blog_system::MySQLInit();
   blog_system::BlogTable blog_table(mysql);
   bool ret=false;
   //测试插入
   Json::Value blog;
-  /*blog["title"]="我的第一篇博客";
+  blog["title"]="我的第一篇博客";
   blog["content"]="我要成为大佬";
   blog["tag_id"]=1;
   blog["create_time"]="2019/12/29";
-  bool ret=blog_table.Insert(blog);
-  printf("insert:%d\n",ret);*/
+  ret=blog_table.Insert(blog);
+  printf("insert:%d\n",ret);
   //测试查找
-  Json::Value blogs;
+  /*Json::Value blogs;
   ret=blog_table.SelectAll(&blogs);
   printf("SelectAll %d\n",ret);
-  printf("blogs:%s\n",writer.write(blogs).c_str());
-
+  printf("blogs:%s\n",writer.write(blogs).c_str());*/
   //测试查找单个博客
   /*ret=blog_table.SelectOne(1,&blog);
   printf("select one %d\n",ret);
