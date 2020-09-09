060e4c8e573cbadcb7899abe7e0c2c5c78c828ad
LMJian <1150696681@qq.com>
2020-09-09 21:48:16 +0800

    9-9-更新oj项目

---
+++ Linux/oj-project/src/temp_files/tmp_1599633743.1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    bool isPalindrome(int x){
+         return true;
+    }
+};
+void Test1(const Solution& s){
+    bool ret = s.isPalindrome(121);
+    if(ret){
+        std::cout<<"Test1 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test1 failed"<<std::endl;
+    }
+}
+
+void Test2(const Solution& s){
+    bool ret = s.isPalindrome(-121);
+    if(!ret){
+        std::cout<<"Test2 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test2 failed"<<std::endl;
+    }
+}
+
+void Test3(const Solution& s){
+    bool ret = s.isPalindrome(10);
+    if(!ret){
+        std::cout<<"Test3 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test3 failed"<<std::endl;
+    }
+}
+
+int main(){
+    Solution s;
+    Test1(s);
+    Test2(s);
+    Test3(s);
+    return 0;
+}
---
+++ Linux/oj-project/src/temp_files/tmp_1599634170.2.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    bool isPalindrome(int x){
+         string a = to_string(x);
+         string b(a.rbegin(),a,rend());
+         return a==b;
+    }
+};
+void Test1(Solution& s){
+    bool ret = s.isPalindrome(121);
+    if(ret){
+        std::cout<<"Test1 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test1 failed"<<std::endl;
+    }
+}
+
+void Test2(Solution& s){
+    bool ret = s.isPalindrome(-121);
+    if(!ret){
+        std::cout<<"Test2 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test2 failed"<<std::endl;
+    }
+}
+
+void Test3(Solution& s){
+    bool ret = s.isPalindrome(10);
+    if(!ret){
+        std::cout<<"Test3 OK"<<std::endl;
+    }
+    else{
+        std::cout<<"Test3 failed"<<std::endl;
+    }
+}
+
+int main(){
+    Solution s;
+    Test1(s);
+    Test2(s);
+    Test3(s);
+    return 0;
+}
